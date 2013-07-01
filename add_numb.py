a3c62daabd4eeebe6e7e0c08bc69f7ba40c89e58
Ben Smith <benjamin.smith@ou.edu>
2013-07-01 15:37:21 -0400

    more adding fun

--- add_numb.py
+++ add_numb.py
@@ -2,9 +2,10 @@
 #Ben's first python program
 def add_two(a,b);
 	return a+b
-
+	print("Adding numbers for fun")
 def main();
-	print ("2 + 3 = ",add_two(2.3))
+	print("2 + 3 = ",add_two(2.3))
+	print("4 + 4 = +,add_two(4,4))
 
 if__name__main == "__main__";
 	main()
